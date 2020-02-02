16812c16686be7230c27930e53e881f622faf7fd
Nate August <n.baker.august@gmail.com>
2020-02-02 09:39:14 -0500

    init cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For statistical analysis on American Ultimate Disc League (AUDL)',
+    author='Nate August (n.baker.august@gmail.com)',
+    license='MIT',
+)
